11f1076992aefbf532247d6d2d906d8e2bd1d13c
Jonathan Kwak <jkwaktdchristian@gmail.com>
2025-08-07 14:51:46 -0400

    more functionality regarding checkboxes

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 $(".checkbox").click(function() {
-    console.log("TRUE!!!")
-    $(this).siblings(".secondary-text").toggleClass("strikethrough");
+    //$(this).siblings(".secondary-text").toggleClass("strikethrough");
     $(this).toggleClass("completed-checkbox");
+
+    if ($(this).hasClass("completed-checkbox")) {
+        $(this).text("X")
+    } else {
+        $(this).text("O")
+    }
 });
